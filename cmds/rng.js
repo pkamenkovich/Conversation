ad4a1ac662fba19b65c462e7dbc3ab844be19c5c
Peter Kamenkovich <pak15@miami.edu>
2018-01-23 14:02:23 -0500

    Styling

--- cmds/rng.js
+++ cmds/rng.js
@@ -4,11 +4,11 @@ module.exports.run = async (bot, message, args) => {
     let max = args[0];
     let solution = 0;
 
-    if(!parseInt(max))
+    if(!parseInt(max) || max >= 9007199254740992)
         return message.channel.send("Error, try again.");
 
     solution = Math.floor(Math.random() * max);
-    message.channel.send(`A random number was generated from 0 to ${max}, the result is ${solution}`);
+    message.channel.send(`A random number was generated from 0 to ${max}: \`\`\`${solution}\`\`\``);
 }
 
 module.exports.help = {
