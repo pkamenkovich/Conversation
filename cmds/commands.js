bfa9e7287eb5582466dace3c7a76423d03a79bbd
Peter Kamenkovich <pak15@miami.edu>
2018-01-25 14:35:07 -0500

    Created a more comprehensive list of commands

--- cmds/commands.js
+++ cmds/commands.js
@@ -2,20 +2,39 @@ const Discord = module.require('discord.js');
 const fs = module.require('fs');
 
 module.exports.run = async (bot, message, args) => {
-    let embed = await new Discord.RichEmbed()
+
+    let intro = await new Discord.RichEmbed()
+        .setThumbnail(bot.user.avatarURL)
+        .setTitle("Conversation Commands")
+        .setDescription("Use the prefix c! before each command to trigger the bot. There are currently two subsections.");
+    
+    message.channel.send({ embed: intro});
+
+    //MODERATION COMMANDS
+    let moderation = await new Discord.RichEmbed()
             .setThumbnail(bot.user.avatarURL)
+            .setTitle("Moderation")
             .setColor("#0066cc")
-            .addField("Moderation:", `List of all functions pertaining to Moderation.`)
-            .addBlankField()
             .addField("ban @mention reason", `Bans the user mentioned.`)
             .addField("kick @mention reason", `Kicks the user mentioned.`)
             .addField("mute @mention time(seconds)", `Mutes the user mentioned for duration provided, or indefinite if time is empty.`)
             .addField("unmute @mention", `Unmutes the user mentioned.`)
-            .setTitle("Conversation Commands")
-            .setDescription("Use the prefix c! before each command to trigger the bot.")
-            .setFooter(`${bot.user.username}'s list of commands.`);
+            .setFooter(`${bot.user.username}'s list of moderation commands.`);
 
-        message.channel.send({ embed: embed });
+    message.channel.send({ embed: moderation });
+
+    //RNG COMMANDS
+    let rng = await new Discord.RichEmbed()
+            .setThumbnail(bot.user.avatarURL)
+            .setTitle("Random Number Generators")
+            .setColor("#0066cc")
+            .addField("rng number", `Generates a number between 0 and the number mentioned.`)
+            .addField("roll #d#", `Generates a dice roll, the first argument is the number of dice, the second number is how many sides the dice has.`)
+            .addField("roll stats", `Creates a 4d6 dice roll, six times. Useful for character creation for DND campaigns.`)
+            .addField("coinflip", `Flips a coin`)
+            .setFooter(`${bot.user.username}'s list of moderation commands.`);
+
+    message.channel.send({ embed: moderation });
 }
 
 module.exports.help = {
