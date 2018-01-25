f99ab3b7d2be7c1194f82638f6c0c004a41b9bb1
Peter Kamenkovich <pak15@miami.edu>
2018-01-25 13:59:26 -0500

    Updated commands.js to include moderation commands.

--- cmds/commands.js
+++ cmds/commands.js
@@ -2,19 +2,20 @@ const Discord = module.require('discord.js');
 const fs = module.require('fs');
 
 module.exports.run = async (bot, message, args) => {
+    let embed = await new Discord.RichEmbed()
+            .setThumbnail(bot.user.avatarURL)
+            .setColor("#0066cc")
+            .addField("Moderation:", `List of all functions pertaining to Moderation.`)
+            .addBlankField()
+            .addField("ban @mention reason", `Bans the user mentioned.`)
+            .addField("kick @mention reason", `Kicks the user mentioned.`)
+            .addField("mute @mention time(seconds)", `Mutes the user mentioned for duration provided, or indefinite if time is empty.`)
+            .addField("unmute @mention", `Unmutes the user mentioned.`)
+            .setTitle("Conversation Commands")
+            .setDescription("Use the prefix c! before each command to trigger the bot.")
+            .setFooter(`${bot.user.username}'s list of commands.`);
 
-    let jsFiles = [];
-
-    fs.readdir("./cmds", (err, files) => {
-        if(err) console.error(err);
-    
-        jsFiles = files.filter(f => f.split(".").pop() === "js");
-
-        jsFiles.forEach(element => {
-            message.channel.send(`\`${element}\``);
-        });
-    });
-
+        message.channel.send({ embed: embed });
 }
 
 module.exports.help = {
